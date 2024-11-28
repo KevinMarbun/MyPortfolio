f08ea2e4017fbe3f4b7382b34222f1990f6dc5be
VinnMarbun11 <vinnjuniardymarbun>
2024-11-28 10:51:30 +0700

    wa add

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,23 @@
+// Fungsi untuk mengirim pesan WhatsApp
+function sendWhatsAppMessage() {
+  const button = document.getElementById("whatsapp-contact-button");
+
+  try {
+    const phoneNumber = button.getAttribute("data-phone-number");
+    const defaultMessage = button.getAttribute("data-default-message");
+
+    const waUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(defaultMessage)}`;
+    window.open(waUrl, "_blank");
+  } catch (error) {
+    console.error(error);
+    errorMessage.style.display = "block";
+    errorMessage.textContent = error.message;
+  }
+}
+
+// Tambahkan event listener
+document.getElementById("whatsapp-contact-button").addEventListener("click", sendWhatsAppMessage);
+
 // navbar fixed
 
 window.onscroll = function () {
